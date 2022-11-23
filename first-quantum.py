0ba16a14dba51a3ce484b085513f24a3c02e3d60
jiaming <yejiaming20@126.com>
2022-11-23 14:30:41 +0900

    init

---
+++ first-quantum.py
@@ -0,0 +1,20 @@
+from z3 import *
+
+k1 = Int('k1')
+a = ((1,k1),(3,4))
+b = ((1,1),(1,1))
+c = ((3,3),(7,7))
+
+s = Solver()
+eq1 = a[0][1]>0
+eq2 = [[sum(a[i][k]*b[k][j] for k in range(2)) == c[i][j] for i in range(2)] for j in range(2)]
+s.add(eq1)
+s.add(eq2[0][0])
+s.add(eq2[0][1])
+s.add(eq2[1][0])
+s.add(eq2[1][1])
+
+print(s)
+print(s.check())
+m = s.model()
+print(m)
